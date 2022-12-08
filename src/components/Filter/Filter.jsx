334ffbb7d5107d59ceb3f2e89836e59e1826afeb
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-12-09 00:32:27 +0100

    add src

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,31 @@
+import styles from './Filter.module.css';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { handleFilter } from 'redux/contacts/contactsSlice';
+
+export const Filter = () => {
+  const [filter, setFilter] = useState('');
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(handleFilter(filter));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [filter]);
+
+  const handleChange = evt => {
+    setFilter(evt.target.value);
+  };
+
+  return (
+    <label className={styles.label}>
+      Find contacts by Name
+      <input
+        className={styles.input}
+        type="text"
+        name="filter"
+        value={filter}
+        onChange={handleChange}
+      />
+    </label>
+  );
+};
