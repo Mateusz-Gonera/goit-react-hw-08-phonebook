dae3dff5a9820865c825e84937b170653188798e
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-12-09 20:00:58 +0100

    add navigation

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,17 +1,27 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Loader } from 'components/Loader/Loader';
-import { Navbar, Container } from 'react-bootstrap';
+import { Container, Button, Navbar } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 const Layout = () => {
   return (
     <>
-      <Navbar>
-              <Container>
-                  
-        </Container>
-      </Navbar>
+      <Container>
+        <Navbar className="px-3 py-3" bg="light">
+          <LinkContainer to="/">
+            <Button variant="secondary">Home</Button>
+          </LinkContainer>
+          <Navbar.Collapse className="justify-content-end">
+            <LinkContainer className="me-2" to="register">
+              <Button variant="secondary">Register</Button>
+            </LinkContainer>
+            <LinkContainer to="login">
+              <Button variant="secondary">Login</Button>
+            </LinkContainer>
+          </Navbar.Collapse>
+        </Navbar>
+      </Container>
       <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
