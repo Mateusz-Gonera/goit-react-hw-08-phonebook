28a617027f1d59b4e96a4d3f8aa568f2ae6c88e4
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-12-13 22:50:02 +0100

    add auth redux

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { App } from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
